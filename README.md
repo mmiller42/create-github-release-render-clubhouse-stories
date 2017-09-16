2cafbf1d6580a735a7d6d8d147065665379f113c
Matt Miller <matt.miller@brightbytes.net>
2017-09-16 15:37:09 -0600

    Project setup

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # create-github-release-render-clubhouse-stories
-Render function for use with create-github-release to render pull requests with associated Clubhouse stories.
+
+Render function for use with [`create-github-release`](https://github.com/mmiller42/create-github-release) to render pull requests with associated Clubhouse stories.
+
+Extends the default template to render another column in the pull requests table with details about any Clubhouse stories that were referenced in the pull request comments by URL.
+
+## Installation
+
+```bash
+npm install --save-dev create-github-release-render-clubhouse-stories
+```
+
+## API
+
+### Configuration
+
+The configuration file must export an object, which can have the following properties:
+
+| Property   | Type     | Description                                                                                    | Default                                                  |
+| ---------- | -------- | ---------------------------------------------------------------------------------------------- | -------------------------------------------------------- |
+| `token`    | string   | Your Clubhouse API token. [Generate one](https://app.clubhouse.io/settings/account/api-tokens) | *Required*                                               |
+| `template` | string   | The path to a Mustache template that will be used to generate the release notes.               | [`DEFAULT_TEMPLATE`](src/defaultTemplate.md.hbs)         |
+
+### Example
+
+```js
+// github-release.config.js
+const createRenderer = require('create-github-release-render-clubhouse-stories')
+
+module.exports = {
+  authenticateOptions: {
+    type: 'oauth',
+    token: 'b5ef45e4a2c245a5a4243b2882034a9f',
+  },
+  owner: 'mmiller42',
+  repo: 'html-webpack-externals-plugin',
+  render: createRenderer({
+    token: '4f4e84744f9f47788468fc1f2d570329',
+  }),
+}
+```
---
+++ src/index.js
@@ -0,0 +1,109 @@
+import path from 'path'
+import { readFile as _readFile } from 'fs'
+import promisify from 'es6-promisify'
+import Mustache from 'mustache'
+import linkifyIt from 'linkify-it'
+import Clubhouse from 'clubhouse-lib'
+import flatten from 'lodash.flatten'
+import isObject from 'lodash.isobject'
+import forEach from 'lodash.foreach'
+
+const readFile = promisify(_readFile)
+const linkify = linkifyIt()
+const templateCache = {}
+const projectCache = {}
+
+export const DEFAULT_TEMPLATE = path.resolve(__dirname, 'defaultTemplate.md.hbs')
+
+export default config => {
+  if (!isObject(config)) {
+    throw new Error('config must be an object')
+  }
+  ;['token'].forEach(property => {
+    if (!config[property]) {
+      throw new Error(`${property} property is required`)
+    }
+  })
+  forEach({ token: 'string', template: 'string' }, (type, property) => {
+    const value = config[property]
+    if (value !== undefined && typeof value !== type) {
+      throw new Error(`${property} property must be of type ${type}`)
+    }
+  })
+
+  const { token, template: templatePath = DEFAULT_TEMPLATE } = config
+
+  const clubhouse = Clubhouse.create(token)
+
+  return view => render(clubhouse, templatePath, view)
+}
+
+const render = async (clubhouse, templatePath, { pullRequests, ...view }) => {
+  let fetchingTemplate = templateCache[templatePath]
+  if (!fetchingTemplate) {
+    fetchingTemplate = readFile(templatePath, 'utf8')
+    templateCache[templatePath] = fetchingTemplate
+  }
+
+  const template = await fetchingTemplate
+  const stories = await Promise.all(
+    pullRequests.map(pullRequest => fetchStories(clubhouse, pullRequest))
+  )
+
+  return Mustache.render(template, {
+    ...view,
+    pullRequests: pullRequests.map((pullRequest, i) => ({
+      ...pullRequest,
+      stories: stories[i],
+    })),
+    storyIcon() {
+      return {
+        feature: ':eight_spoked_asterisk:',
+        bug: ':bug:',
+        chore: ':wrench:',
+      }[this.type]
+    },
+  })
+}
+
+const fetchStories = async (clubhouse, pullRequest) => {
+  return await Promise.all(
+    extractStoryRefs(pullRequest).map(storyRef => fetchStory(clubhouse, storyRef))
+  )
+}
+
+const extractStoryRefs = pullRequest => {
+  const urlPattern = /^https:\/\/app\.clubhouse\.io\/.*\/story\/([0-9]+)\/.*$/
+  return [pullRequest.body, ...flatten(pullRequest.comments.map(comment => comment.body))]
+    .map(body => linkify.match(body) || [])
+    .map(({ url }) => {
+      const match = url && url.match(urlPattern)
+      if (match) {
+        const [url, id] = match
+        return { url, id }
+      } else {
+        return null
+      }
+    })
+    .filter(storyRef => storyRef)
+}
+
+const fetchStory = async (clubhouse, { url, id }) => {
+  const storyData = await clubhouse.getStory(id)
+
+  let fetchingProject = projectCache[storyData.project_id]
+  if (!fetchingProject) {
+    fetchingProject = clubhouse.getProject(storyData.project_id)
+    projectCache[storyData.project_id] = fetchingProject
+  }
+
+  const projectData = await fetchingProject
+
+  return {
+    id,
+    url,
+    title: storyData.name,
+    type: storyData.story_type,
+    project: projectData.abbreviation,
+  }
+}
